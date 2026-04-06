1d2077960c01a9931c962e19d082aa6de680ac18
Akshay KM <akshay@aventusinformatics.in>
2026-04-06 18:48:08 +0530

    robin

--- chunk.c
+++ chunk.c
@@ -1,5 +1,6 @@
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 #include <stdlib.h>
 
@@ -7,11 +8,17 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
+}
+
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
+  initChunk(chunk);
 }
 
 void writeChunk(Chunk *chunk, uint8_t byte) {
-
-  if (chunk->capacity <= chunk->count) {
+  if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
@@ -20,4 +27,9 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
 
   chunk->code[chunk->count] = byte;
   chunk->count++;
+}
+
+int addContant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
--- common.h
+++ common.h
@@ -6,5 +6,3 @@
 #include <stdint.h>
 
 #endif
-
-
